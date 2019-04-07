d6573b1e7a34c62811383c49d46cd2fe05534e2d
hzkljy <850316314@qq.com>
2019-04-07 10:08:30 +0800

    实现简单的url到view，view到模板的逻辑，完成基本框架的搭建

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request, category_id=None, tag_id=None):
+    return render(request, 'blog/list.html', context={'name': 'post_list'})
+
+
+def post_detail(request, post_id=None):
+    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
--- blog_sys/settings/base.py
+++ blog_sys/settings/base.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog_sys',
     'blog',
     'config',
     'comment',
