959cf6c2d2664e78ec630ac0ecd3141971d1a59d
hzkljy <850316314@qq.com>
2019-04-07 11:17:28 +0800

    完善模板信息，补充HTML文件必备的信息

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-from .models import Post, Tag
+from .models import Post, Tag, Category
 
 # Create your views here.
 def post_list(request, category_id=None, tag_id=None):
+    tag = None
+    category = None
+
     if tag_id:
         try:
             tag = Tag.objects.get(id=tag_id)
@@ -17,9 +20,20 @@ def post_list(request, category_id=None, tag_id=None):
     else:
         post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
         if category_id:
-            post_list = post_list.filter(category_id=category_id)
+            try:
+                category = Category.objects.get(id=category_id)
+            except Category.DoesNotExist:
+                category = None
+            else:
+                post_list = post_list.filter(category_id=category_id)
 
-    return render(request, 'blog/list.html', context={'post_list': 'post_list'})
+    context = {
+        'category': category,
+        'tag': tag,
+        'post_list': post_list,
+    }
+
+    return render(request, 'blog/list.html', context=context)
 
 
 def post_detail(request, post_id=None):
