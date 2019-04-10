d820b744b779d3fb7b988c22331cedb48f0d3b77
hzkljy <850316314@qq.com>
2019-04-10 22:39:55 +0800

    增加侧边栏展示

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from .models import Post, Tag, Category
+from config.models import SideBar
 
 # Create your views here.
 def post_list(request, category_id=None, tag_id=None):
@@ -21,6 +22,7 @@ def post_list(request, category_id=None, tag_id=None):
         'category': category,
         'tag': tag,
         'post_list': post_list,
+        'sidebars': SideBar.get_all(),
     }
     context.update(Category.get_navs())
 
@@ -34,7 +36,8 @@ def post_detail(request, post_id=None):
         post = None
 
     context = {
-        'post': post
+        'post': post,
+        'sidebars': SideBar.get_all(),
     }
     context.update(Category.get_navs())
 
